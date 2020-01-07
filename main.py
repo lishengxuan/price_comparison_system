0f305a4adf2f257efdc01ab5a3b65e226173c8c0
lishengxuan <lishengxuan@dragonest.com>
2020-01-07 17:58:51 +0800

    登录页面完成

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
 
 app = Flask(__name__)
@@ -9,3 +9,14 @@ app.config.from_object(config)
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    登录
+    """
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return "登录"
